f6935953c32e55bb1083559ed1aea91b1dcef4c1
Natalia <hubska.natalia@lll.kpi.ua>
2026-05-04 13:07:54 -0400

    Fix total amount display

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -101,7 +101,12 @@ const CartItem = ({ onContinueShopping }) => {
         ))}
       </div>
 
-      <div style={{ marginTop: '20px', color: 'black' }} className="total_cart_amount"></div>
+      <div
+      style={{ marginTop: '20px', color: 'black' }}
+      className="total_cart_amount"
+    >
+    Total Cart Amount: ${calculateTotalAmount()}
+    </div>
 
       {/* ✅ ВИПРАВЛЕНИЙ БЛОК */}
       <div className="continue_shopping_btn">
